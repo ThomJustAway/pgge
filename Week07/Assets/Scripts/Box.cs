945b3b7335d75fd79ab0086063220c9ea97fdc72
Thom Low <thomthomlow@gmail.com>
2023-12-05 16:56:34 +0800

    update

---
+++ Week07/Assets/Scripts/Box.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Box : MonoBehaviour , IDamageable
+    {
+        public void TakeDamage()
+        {
+            Debug.Log("Box: I am hit by a bullet!");        
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            TakeDamage();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        
+    }
+}
---
+++ Week07/Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Bullet : MonoBehaviour 
+    {
+        void Start()
+        {
+            // Destroy the bullet after 10 seconds if it does not hit any object.
+            StartCoroutine(Coroutine_Destroy(10.0f));
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            IDamageable obj = collision.gameObject.GetComponent<IDamageable>();
+            if (obj != null)
+            {
+                obj.TakeDamage();
+            }
+            StartCoroutine(Coroutine_Destroy(0.1f));
+        }
+
+        IEnumerator Coroutine_Destroy(float duration)
+        {
+            yield return new WaitForSeconds(duration);
+            Destroy(gameObject);
+        }
+
+    }
+}
